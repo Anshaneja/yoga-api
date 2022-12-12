c29af327f428023f533f8cb08438e461df679343
Anshaneja <anshaneja05@gmail.com>
2022-12-12 23:41:34 +0530

    cors2

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ OurApp.use(
     })
   );
 OurApp.use(bodyparser.json());
-
+OurApp.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("connection established!"))
